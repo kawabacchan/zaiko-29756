caf6ea617c32c735a229420f1d0a8c9aa07baee7
kawabacchan <daisuke30x@gmail.com>
2020-09-23 16:35:55 +0900

    商品のバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,11 @@
 class Item < ApplicationRecord
+
+  validates :category, presence: true
+  validates :name, presence: true, uniqueness:true
+  validates :code, presence: true, uniqueness: true, format: { with: /\A[A-Z]{1}\d{3}\z/, message: "半角英字1文字と半角数字3桁を入力してください"}
+  
+  with_options presence: true, numericality: {greater_than_or_equal_to: 0} do
+    validates :stock, :monthly_sales, :creation_days
+  end
+
 end

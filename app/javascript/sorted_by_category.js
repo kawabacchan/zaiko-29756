9c6fff0de946389344ff4f6916eeea147907bdd4
kawabacchan <daisuke30x@gmail.com>
2020-09-24 14:48:11 +0900

    発注リストの非同期表示の実装

--- app/javascript/sorted_by_category.js
+++ app/javascript/sorted_by_category.js
@@ -1,25 +1,40 @@
 function sort() {
-
-  const sortByCategory = document.getElementById("sort-by-category")
   const sortByCode = document.getElementById("sort-by-code")
+  const sortByCategory = document.getElementById("sort-by-category")
+  const sortCreationList =document.getElementById("sort-creation-list")
   const sortByCodeDisplay = document.getElementById("sort-by-code-display")
   const sortByCategoryDisplay = document.getElementById("sort-by-category-display")
+  const sortCreationListDisplay = document.getElementById("sort-creation-list-display")
 
   sortByCode.setAttribute("style", "background-color: lightblue;");
-  sortByCategoryDisplay.setAttribute("style", "display: none;");
+  sortByCodeDisplay.setAttribute("style", "display: block;");
 
   sortByCategory.addEventListener('click', function(){
     this.setAttribute("style", "background-color: lightblue;");
+    sortByCategoryDisplay.setAttribute("style", "display: block;");
     sortByCode.setAttribute("style", "background-color: white;");
     sortByCodeDisplay.setAttribute("style", "display: none;");
-    sortByCategoryDisplay.setAttribute("style", "display: block;");
+    sortCreationList.setAttribute("style", "background-color: white;");
+    sortCreationListDisplay.setAttribute("style", "display: none;");
   })
 
   sortByCode.addEventListener('click', function(){
     this.setAttribute("style", "background-color: lightblue;");
+    sortByCodeDisplay.setAttribute("style", "display: block;");
     sortByCategory.setAttribute("style", "background-color: white;");
     sortByCategoryDisplay.setAttribute("style", "display: none;");
-    sortByCodeDisplay.setAttribute("style", "display: block;");
+    sortCreationList.setAttribute("style", "background-color: white;");
+    sortCreationListDisplay.setAttribute("style", "display: none;");
   })
+
+  sortCreationList.addEventListener('click', function(){
+    this.setAttribute("style", "background-color: lightblue;");
+    sortCreationListDisplay.setAttribute("style", "display: block;");
+    sortByCategory.setAttribute("style", "background-color: white;");
+    sortByCategoryDisplay.setAttribute("style", "display: none;");
+    sortByCode.setAttribute("style", "background-color: white;");
+    sortByCodeDisplay.setAttribute("style", "display: none;");
+  })
+
 }
 window.addEventListener('load', sort)

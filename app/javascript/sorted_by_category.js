47b2e50c732f63ca4a471814d38b68f72c60ce39
kawabacchan <daisuke30x@gmail.com>
2020-09-24 17:13:37 +0900

    コード順表示ボタンをデフォルトで水色に変更、一覧表示ボタンを押して再度イベント発火するようにsetIntervalへ変更

--- app/javascript/sorted_by_category.js
+++ app/javascript/sorted_by_category.js
@@ -6,35 +6,32 @@ function sort() {
   const sortByCategoryDisplay = document.getElementById("sort-by-category-display")
   const sortCreationListDisplay = document.getElementById("sort-creation-list-display")
 
-  sortByCode.setAttribute("style", "background-color: lightblue;");
-  sortByCodeDisplay.setAttribute("style", "display: block;");
-
   sortByCategory.addEventListener('click', function(){
-    this.setAttribute("style", "background-color: lightblue;");
-    sortByCategoryDisplay.setAttribute("style", "display: block;");
-    sortByCode.setAttribute("style", "background-color: white;");
-    sortByCodeDisplay.setAttribute("style", "display: none;");
-    sortCreationList.setAttribute("style", "background-color: white;");
-    sortCreationListDisplay.setAttribute("style", "display: none;");
+  this.setAttribute("style", "background-color: lightblue;");
+  sortByCategoryDisplay.setAttribute("style", "display: block;");
+  sortByCode.setAttribute("style", "background-color: white;");
+  sortByCodeDisplay.setAttribute("style", "display: none;");
+  sortCreationList.setAttribute("style", "background-color: white;");
+  sortCreationListDisplay.setAttribute("style", "display: none;");
   })
 
   sortByCode.addEventListener('click', function(){
-    this.setAttribute("style", "background-color: lightblue;");
-    sortByCodeDisplay.setAttribute("style", "display: block;");
-    sortByCategory.setAttribute("style", "background-color: white;");
-    sortByCategoryDisplay.setAttribute("style", "display: none;");
-    sortCreationList.setAttribute("style", "background-color: white;");
-    sortCreationListDisplay.setAttribute("style", "display: none;");
+  this.setAttribute("style", "background-color: lightblue;");
+  sortByCodeDisplay.setAttribute("style", "display: block;");
+  sortByCategory.setAttribute("style", "background-color: white;");
+  sortByCategoryDisplay.setAttribute("style", "display: none;");
+  sortCreationList.setAttribute("style", "background-color: white;");
+  sortCreationListDisplay.setAttribute("style", "display: none;");
   })
 
   sortCreationList.addEventListener('click', function(){
-    this.setAttribute("style", "background-color: lightblue;");
-    sortCreationListDisplay.setAttribute("style", "display: block;");
-    sortByCategory.setAttribute("style", "background-color: white;");
-    sortByCategoryDisplay.setAttribute("style", "display: none;");
-    sortByCode.setAttribute("style", "background-color: white;");
-    sortByCodeDisplay.setAttribute("style", "display: none;");
+  this.setAttribute("style", "background-color: lightblue;");
+  sortCreationListDisplay.setAttribute("style", "display: block;");
+  sortByCategory.setAttribute("style", "background-color: white;");
+  sortByCategoryDisplay.setAttribute("style", "display: none;");
+  sortByCode.setAttribute("style", "background-color: white;");
+  sortByCodeDisplay.setAttribute("style", "display: none;");
   })
-
 }
-window.addEventListener('load', sort)
+
+setInterval(sort, 1000)

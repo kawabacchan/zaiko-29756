62d39b5bc5e0ed39769583eb9642825eebe7e285
kawabacchan <daisuke30x@gmail.com>
2020-09-25 12:20:15 +0900

    店舗一覧の画面の表示

---
+++ app/controllers/shops_controller.rb
@@ -0,0 +1,22 @@
+class ShopsController < ApplicationController
+
+  def index
+    @shops = Shop.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end

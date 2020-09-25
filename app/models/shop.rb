1a68dd9679ec2e3aec56be87b15d30862dceb7c6
kawabacchan <daisuke30x@gmail.com>
2020-09-25 18:58:21 +0900

    エラー修正

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,14 +1,13 @@
 class Shop < ApplicationRecord
 
+  belongs_to :company
   has_many :delivery_dates
   has_many :items, through: :delivery_dates
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
 
-  validates :company_name, presence:true
-  validates :shop_name, presence:true, uniqueness: { case_sensitive: true }
-  validates :shop_code, presence:true, uniqueness: { case_sensitive: true }, format: { with: /\A\d{5}\z/}
+  validates :name, presence:true, uniqueness: { case_sensitive: true }
   validates :prefecture, presence:true
   validates :delivery_days, presence:true, numericality: {other_than: 0}
   validates :prefecture_id, numericality: {other_than: 1}

05086d16de88bfb09cc5f3cea10fd5c998d20139
kawabacchan <daisuke30x@gmail.com>
2020-09-25 15:22:20 +0900

    店舗登録機能の作成

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -10,6 +10,13 @@ class ShopsController < ApplicationController
   end
 
   def create
+    @shops = Shop.all.order(shop_code: "ASC") 
+    @shop = Shop.new(params_shop)
+    if @shop.save
+      redirect_to new_shop_path
+    else
+      render :new
+    end
   end
 
   def edit
@@ -19,6 +26,12 @@ class ShopsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def params_shop
+    params.require(:shop).permit(:company_name, :shop_name, :shop_code, :prefecture_id, :delivery_days)
   end
 
 end

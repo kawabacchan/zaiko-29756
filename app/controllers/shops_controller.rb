40431f198a811522d991233df1e4f1d1a59b487d
kawabacchan <daisuke30x@gmail.com>
2020-09-26 12:46:13 +0900

    店舗編集機能の作成

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,18 +1,15 @@
 class ShopsController < ApplicationController
 
-  def index
-    @shops = Shop.all.order(name: "ASC")
-  end
+  before_action :all_shop, only: [:new, :create, :edit, :update, :destroy]
+  before_action :set_shop, only: [:edit, :update, :destroy]
 
   def new
-    @company = Company.find(params[:company_id])
-    @shops = Shop.all.order(name: "ASC")
     @shop = Shop.new
+    @company = Company.find(params[:company_id])
   end
 
   def create
-    @shops = Shop.all.order(name: "ASC") 
-    @shop = Shop.new(params_shop)
+    @shop = Shop.new(shop_params)
     if @shop.save
       redirect_to new_company_shop_path
     else
@@ -21,18 +18,38 @@ class ShopsController < ApplicationController
   end
 
   def edit
+    @company = Company.find(params[:company_id])
   end
 
   def update
+    @company = Company.find(params[:company_id])
+    if @shop.update(shop_params)
+      redirect_to new_company_shop_path
+    else
+      render :edit
+    end
   end
 
   def destroy
+    if @shop.destroy
+      redirect_to company_shop_path
+    else
+      render :new
+    end
   end
 
   private
 
-  def params_shop
+  def shop_params
     params.require(:shop).permit(:name, :prefecture_id, :delivery_days).merge(company_id: params[:company_id])
+  end
+
+  def all_shop
+    @shops = Shop.all.order(company_id: "ASC")
+  end
+
+  def set_shop
+    @shop = Shop.find(params[:id])
   end
 
 end

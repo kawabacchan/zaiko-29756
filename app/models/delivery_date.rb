6d4971f2c9aac086f2aca668742de68212d67ba9
kawabacchan <daisuke30x@gmail.com>
2020-09-27 18:01:18 +0900

    不要なモデルだと気づいたため削除

--- app/models/delivery_date.rb
+++
@@ -1,6 +0,0 @@
-class DeliveryDate < ApplicationRecord
-
-  belongs_to :item
-  belongs_to :shop
-  
-end
--- db/migrate/20200926091121_create_delivery_dates.rb
+++
@@ -1,9 +0,0 @@
-class CreateDeliveryDates < ActiveRecord::Migration[6.0]
-  def change
-    create_table :delivery_dates do |t|
-      t.references :item, foreign_key: true
-      t.references :shop, foreign_key: true
-      t.timestamps
-    end
-  end
-end

eb8c4c6b9f4cfc94c49ea54dda4fb202fe28efbc
kawabacchan <daisuke30x@gmail.com>
2020-09-27 19:55:13 +0900

    販売登録画面の作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,15 @@
 class OrdersController < ApplicationController
+
+  def index
+  end
+
+  def new
+    @items = Item.all.order(code: "ASC")
+    @shop = Shop.find(params[:shop_id])
+    @order = Order.new
+  end
+
+  def create
+  end
+
 end

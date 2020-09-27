1dd0f874390eeb30bf525558d91dd8130888bf2d
kawabacchan <daisuke30x@gmail.com>
2020-09-27 12:28:42 +0900

    企業モデルの単体テストコードの作成

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Company, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before do
+    @company = FactoryBot.build(:company)
+  end
+
+  describe "企業登録" do
+    context "うまくいく時" do
+      it "nameが存在する時" do
+        expect(@company).to be_valid
+      end
+    end
+
+    context "うまくいかない時" do
+      it "nameが空の時" do
+        @company.name = nil
+        @company.valid?
+        expect(@company.errors.full_messages).to include("Name can't be blank")
+      end
+    end
+  end
 end

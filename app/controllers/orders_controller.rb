2359036f1e716ffd0d5cee88ce21ffaeb33d97ea
kawabacchan <daisuke30x@gmail.com>
2020-09-28 14:26:39 +0900

    注文登録機能の作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,8 @@
 class OrdersController < ApplicationController
 
   def index
+    @items = Item.all.order(code: "ASC")
+    @shop = Shop.find(params[:shop_id])
   end
 
   def new
@@ -10,6 +12,23 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @item = Item.find(params[:item_id])
+    @items = Item.all.order(code: "ASC")
+    @shop = Shop.find(params[:shop_id])
+    @order = Order.new(order_params)
+    if @order.save
+      redirect_to new_company_shop_order_path
+    else
+      render :new
+    end
+  end
+
+
+
+  private
+
+  def order_params
+    params.require(:order).permit(:sales_numbers).merge(shop_id: params[:shop_id], item_id: params[:item_id])
   end
 
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -5,4 +5,11 @@ class Order < ApplicationRecord
 
   validates :sales_numbers, presence: true, numericality: {greater_than: 0}
 
+  def sum
+    self.each do |order|
+      sum += order.sales_numbers
+    end
+    puts sum 
+  end
+
 end

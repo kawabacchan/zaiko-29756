e59de296ef3300037adda49918b0c751e9088dcf
kawabacchan <daisuke30x@gmail.com>
2020-09-29 12:31:10 +0900

    削除機能の作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :items, only: [:index, :new, :create, :edit, :update, :destroy]
   resources :companies, only: [:index, :new, :create, :edit, :update, :destroy] do
     resources :shops, only: [:index, :new, :create, :edit, :update, :destroy] do
-      resources :orders, only: [:index, :new, :create]
+      resources :orders, only: [:index, :new, :create, :destroy]
     end
   end
 

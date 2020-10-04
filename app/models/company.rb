9b91469f58d87a8263bd4b64b941010f6bb73a19
kawabacchan <daisuke30x@gmail.com>
2020-10-04 11:06:31 +0900

    商品コードのバリデーション変更

--- app/models/company.rb
+++ app/models/company.rb
@@ -10,7 +10,6 @@ class Company < ApplicationRecord
       company = self.new(name: row[0].value)
       if row[1].present? && row[2].present? && row[3].present? && Prefecture.where(name: row[2].value).present?
         if self.pluck(:name).include?(company.name)
-
           if Shop.where(company_id: Company.find_by(name: company.name).id).present?
             if Shop.where(company_id: Company.find_by(name: company.name).id).pluck(:name).include?(row[1].value)
               Shop.where(company_id: Company.find_by(name: company.name).id).find_by(name: row[1].value).update(prefecture_id: Prefecture.find_by(name: row[2].value).id, delivery_days: row[3].value, company_id: Company.find_by(name: company.name).id)
@@ -22,7 +21,6 @@ class Company < ApplicationRecord
             shop = Shop.new(name: row[1].value, prefecture_id: Prefecture.find_by(name: row[2].value).id, delivery_days: row[3].value, company_id: Company.find_by(name: company.name).id)
             shop.save
           end
-
         else
           company.save
           shop = Shop.new(name: row[1].value, prefecture_id: Prefecture.find_by(name: row[2].value).id, delivery_days: row[3].value, company_id: company.id)
--- app/models/item.rb
+++ app/models/item.rb
@@ -8,7 +8,7 @@ class Item < ApplicationRecord
   validates :category, presence: true
   validates :category_id, numericality: {other_than: 1}
 
-  validates :code, presence: true, uniqueness: { case_sensitive: true }, format: { with: /\A[A-Z]{1}\d{3}\z/}
+  validates :code, presence: true, uniqueness: { case_sensitive: true }, format: { with: /\A[a-zA-Z0-9]+\z/}
   validates :name, presence: true, uniqueness: { case_sensitive: true }
   
   with_options presence: true, numericality: {greater_than_or_equal_to: 0} do

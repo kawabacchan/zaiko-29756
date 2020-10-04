35446766ddc2dce47929dd2e30f0449d9bd1cf39
kawabacchan <daisuke30x@gmail.com>
2020-10-04 17:07:31 +0900

    店舗のテストコード作成

--- spec/factories/shops.rb
+++ spec/factories/shops.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :shop do
 
-    name {Faker::Name.name}
-    prefecture_id {Faker::Number.between(from:1, to:10)}
-    delivery_days {Faker::Number.between(from:1, to:10)}
+    name {Faker::Name.first_name}
+    prefecture_id {Faker::Number.between(from:2, to:48)}
+    delivery_days {Faker::Number.between(from:1, to:99)}
     association :company
 
   end
